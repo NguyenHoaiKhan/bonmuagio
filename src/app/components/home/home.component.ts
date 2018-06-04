fe2849d9b72363e5375f4c8989db53931e0b3b9c
nguyenhoaikhan <hoaikhan170797@gmail.com>
2018-06-04 09:43:25 +0700

    create function set days of calendar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,10 +7,25 @@ import {EventService} from '../../services/event.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
   constructor(private userService: EventService) {
     userService.getAll().subscribe(users => console.log(users));
     userService.getById({_id: '5b0ef89a73434c0c4840899c'}).subscribe(user => console.log(user));
+    console.log(this.dayOfWeek);
+    console.log(this.numberOfDays);
+    this.setArrayofCanlendar();
+  }
+
+  days: string[] = [];
+  dayOfWeek: number = new Date('7/1/2018').getDay();
+  numberOfDays: number = new Date(2018, 7, 0).getDate();
+
+  setArrayofCanlendar() {
+    for (let i = 1; i <= 35; i++) if (i <= this.dayOfWeek || i > this.numberOfDays + this.dayOfWeek) {
+      this.days.push(' ');
+    }
+    else {
+      this.days.push(`${i - this.dayOfWeek }`)
+    }
   }
   ngOnInit() {
   }
